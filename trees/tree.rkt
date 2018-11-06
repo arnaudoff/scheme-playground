680db16b4820778eaad17d00fd4ceb60ba5537b2
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2018-11-06 15:56:56 +0200

    Fix tree

--- trees/tree.rkt
+++ trees/tree.rkt
@@ -19,19 +19,19 @@
   (and (empty-tree? (left-tree tree))
        (empty-tree? (right-tree tree))))
 
-(define (tree-member? tree x)
-  (cond [(empty-tree? tree) #f]
-        [(= x (root tree)) #t]
-        [else
-          (or (tree-member? (left-tree tree) x)
-              (tree-member? (right-tree tree) x))]))
+(define (member-tree? x t)
+  (cond [(empty-tree? t) #f]
+        [(= x (root t)) #t]
+        [else (or (member-tree? x (left-tree t))
+                  (member-tree? x (right-tree t)))]))
 
-(define (depth tree)
-  (if (empty-tree? tree)
-    0
-    (+ 1 (max (depth (left-tree tree))
-              (depth (right-tree tree))))))
+(define (depth t)
+  (if (empty-tree? t) 0
+    (+ 1 (max (depth (left-tree t))
+              (depth (right-tree t))))))
 
-(define tree (make-tree 1 (make-tree 2 4 5) (make-tree 3 6 7)))
-
-(tree-member? tree 7)
+(define (member_leaf? x t)
+  (cond [(empty-tree? t) #f]
+        [(leaf? t) (= x (root t))]
+        [else (or (member_leaf? x (left-tree t))
+                   (member_leaf? x (right-tree t)))]))
